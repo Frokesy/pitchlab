d51ad7f4ae62bbb82690c5f04206e29372a9fed8
Ayanfeoluwa Noah Akindele <akindeleayanfeoluwa@gmail.com>
2026-03-13 02:53:42 +0100

    project reinitialized

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="pt-6">lfg</div>;
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import contentCollections from '@content-collections/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-const config = defineConfig({
-  plugins: [
-    devtools(),
-    contentCollections(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
-
-export default config
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
